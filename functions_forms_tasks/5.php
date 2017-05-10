ae8980174c8d6fb0b45621723e95483389079e25
Denis <lancelot8097@gmail.com>
2017-05-10 08:58:29 +0300

    fix 5 token

--- functions_forms_tasks/5.php
+++ functions_forms_tasks/5.php
@@ -10,65 +10,61 @@
 
 
 
-function myScanDir($dir, $in_name, $in_text)
+
+
+
+
+function myScanDir($dir, $inName = null, $inText = null)
 {
-    //Проверяем указан ли путь и существует-ли такая дериктория
-    if($dir === null){
-        return '<p>Вы забыли указать где искать !.</p>';
-    }elseif (!is_dir($dir)){
-        return '<p>Я не смог найти такую дерикторию !.</p>';
-    }
+    if(!empty($dir)){
+        //Добовляем замыкающий слэш если его нет
+        if($dir{mb_strlen($dir)-1}!=='\\') $dir.='\\';
 
-    //Добовляем замыкающий слэш если его нет
-    if($dir{mb_strlen($dir)-1}!=='\\') $dir.='\\';
+        if(file_exists($dir)){
+            //подгружаем каталог
+            $ls = scandir($dir);
+            $result = [];
 
-    //Подгружаем листинг раздела
-    $ls = scandir($dir);
-    $result = [];
-
-    //Фильтруем по имени
-    if($in_name !== null){
-        foreach ($ls as $line){
-            //отбрасываем линки верх в низ
-            if($line == '.' || $line == '..') continue;
-            //Если это не файл то пропускамем
-            if(!is_file($dir.$line)) continue;
-
-            //проверяем имя
-            if(preg_match('#.*('.$in_name.'){1}.*#u', $line)) $result[] = $line;
-        }
-
-        //Обновляем листинг
-        $ls = $result;
-        $result = [];
-    }
-
-    //Фильтруем по содержимому
-    if($in_text !== null){
-        foreach ($ls as $line){
-            //отбрасываем линки верх в низ
-            if($line == '.' || $line == '..') continue;
-            //Если это не файл то пропускамем
-            if(!is_file($dir.$line)) continue;
-            //проверяем на соответствие
-            if(strpos(file_get_contents($dir.$line),$in_text)){
+            //Отбрасываем то что не надо
+            foreach ($ls as $line){
+                if($line == '.' || $line == '..' || is_dir($dir.$line)) continue;
                 $result[] = $line;
             }
-        }
-        $ls = $result;
-    }
+            //Так как работаем в процедурном стиле и есть несколько условий, будим перезаписывать обновлённые значения
+            $ls = $result;
+            $result = [];
 
-    if(count($ls)){
-        $result.= '<p>Найдено '.count($ls).' совпадений.</p>';
-        $result.= '<ul>';
-        foreach ($ls as $line){
-            $result.= '<li>'.$line.'</li>';
-        }
-        $result.= '</ul>';
+            //Фильтруем по имени
+            if(!empty($inName)){
+                foreach ($ls as $line){
+                    if(preg_match('#.*('.$inName.').*#ui', $line)){
+                        $result[] = $line;
+                    }
+                }
 
-        return $result;
+                $ls = $result;
+                $result = [];
+            }
+
+            //Фильтруем по содержимому
+            if(!empty($inText)){
+                foreach ($ls as $line){
+                    if(strpos(file_get_contents($dir.$line),$inText)){
+                        $result[] = $line;
+                    }
+                }
+                $ls = $result;
+            }
+            unset($result);
+
+            return $ls;
+
+        }else{
+            return 'Директория <b>'.$dir.'</b> не существует.';
+        }
     }else{
-        return '<p>Нет совпадений.</p>';
+        return 'Путь не указан';
+
     }
 }
 
@@ -78,7 +74,18 @@ if(!empty($_POST)){
     $in_name = (!empty($_POST['in_name'])) ?trim($_POST['in_name']): null;
     $in_text = (!empty($_POST['in_text'])) ?trim($_POST['in_text']): null;
 
-    echo myScanDir($dir, $in_name, $in_text);
+    if(is_array($files = myScanDir($dir, $in_name, $in_text))){
+
+        if(count($files)){
+            foreach ($files as $key => $file){
+                echo '<p>'.++$key.': '.$file.'</p>';
+            }
+        }else{
+            echo '<h5>Совпадений не найдено</h5>';
+        }
+    }else{
+        echo $files;
+    };
 }
 ?>
 
